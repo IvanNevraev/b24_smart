090abb166b323b64a1133b84566a0ff2a94de0a2
Nevraev Ivan <nevraev@mail.ru>
2025-04-22 14:27:53 +0500

    Smart Версия 0.0.2

--- local/modules/chelbit.smart/install/index.php
+++ local/modules/chelbit.smart/install/index.php
@@ -10,7 +10,7 @@ class chelbit_smart extends CModule
             'VERSION_DATE' => '2024-12-20 14:36:00'
         ];
         $this->MODULE_ID = 'chelbit.smart';
-        $this->MODULE_VERSION = "0.0.1";
+        $this->MODULE_VERSION = "0.0.2";
         $this->MODULE_VERSION_DATE = '2025-04-11 14:23:00';
         $this->MODULE_NAME = 'Первый бит - Умный модуль';
         $this->MODULE_DESCRIPTION  = 'Модуль для работы со смарт-процессами и не только...';
